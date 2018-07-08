4a9ab4ec630a6a8e8f32466b1e177e4e8f46a6d4
srattha <srattha.s@gmail.com>
2018-07-08 19:48:11 +0700

    .

--- resources/views/admin/main_navbar.blade.php
+++ resources/views/admin/main_navbar.blade.php
@@ -1,7 +1,7 @@
 <!-- Main navbar -->
 <div class="navbar navbar-inverse navbar-fixed-top header-highlight">
     <div class="navbar-header">
-        <a class="navbar-brand" href="/"><img src="{{ asset('assets/images/logo_light.png') }}" alt=""></a>
+        <!-- <a class="navbar-brand" href="/"><img src="/" alt=""></a>LOGO -->
 
         <ul class="nav navbar-nav pull-right visible-xs-block">
             <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-tree5"></i></a></li>
--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -4,10 +4,8 @@
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<!-- CSRF Token -->
-	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>Cakebox Signage</title>
-	<!-- Global stylesheets -->
+
+	<title>Laravel</title>
 	<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'/>
 	<link href="{{ asset('assets/css/icons/icomoon/styles.css') }}" rel="stylesheet" type="text/css">
 	<link href="{{ asset('assets/css/bootstrap.css') }}" rel="stylesheet" type="text/css">
